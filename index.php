1f8238b7a931277a487b6548bc5b920c4d0978a0
Thiméo Tonon <ttonon@ensc.fr>
2024-11-25 22:48:33 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'Accueil - TONON Thiméo</title>
+</head>
+<body>
+    <h1>Espace de dépôt - Thiméo TONON</h1>
+    <h3>Liste des projets web</h3>
+    <ul>
+        <li><a href="pages/travail_projet.html">Projet site Web 1A - Bases de Données et Programmation Web</a></li>
+    </ul>
+</body>
+</html>
